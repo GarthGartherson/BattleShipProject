63296a180f90e8e7bb4243e9ae97fc398a1e8ff7
Garrett <garrettgreeves@gmail.com>
2022-02-17 22:14:46 +0700

    Added Rotating Functionality, and Reset Button. Added Some Buttons

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import regeneratorRuntime from "regenerator-runtime";
 
 import "./testSuite/shipArray.js";
 import shipArray from "./testSuite/shipArray.js";
+
+// buttons
+const rotateShipButton = document.querySelector(".button-rotate-ship");
+const startGameButton = document.querySelector(".button-start-game");
+const resetGameButton = document.querySelector(".button-reset-game");
+
+// Game Boards
 const playerGameBoard = document.querySelector(".player-board");
-const computerGameBoard = document.querySelector(".computer-board");
+const computerGameBozard = document.querySelector(".computer-board");
+const shipContainer = document.querySelector(".ship-container");
 
 // Ship Size Selectors
 const defaultCarrier = document.querySelector(".defaultCarrier");
@@ -15,31 +23,104 @@ const defaultDestroyer = document.querySelector(".defaultDestroyer");
 const defaultPatrolboat = document.querySelector(".defaultPatrolboat");
 
 const playerGameBoardObject = gameBoardFactory(10);
+const draggables = document.querySelectorAll(".draggable");
+let placementDirection = true;
+
+resetGameButton.addEventListener("click", (e) => {
+  playerGameBoardObject.grid.forEach((tile, index) => {
+    console.log(tile);
+    if (tile !== "w") {
+      playerGameBoardObject.grid[index] = "w";
+    }
+    tile = "w";
+  });
+
+  e.preventDefault();
+  const playerBoardSpaces = document.querySelectorAll(
+    ".player-board > .container"
+  );
+
+  playerBoardSpaces.forEach((space) => {
+    space.style.backgroundColor = "#d4f1f9";
+    console.log(space.hasChildNodes());
+    if (space.hasChildNodes === true) {
+      const element = space.querySelector(".draggable");
+      console.log(element);
+      space.remove(element);
+      space.style.backgroundColor = "";
+    }
+  });
+
+  draggables.forEach((draggable) => {
+    draggable.draggable = true;
+
+    if (!draggable.closest(".container").classList.contains("default")) {
+      draggable.style.backgroundColor = "grey";
+      let firstCapitalized =
+        "default" +
+        draggable.classList[1].split("")[0].toUpperCase() +
+        draggable.classList[1].slice(1);
+      document.querySelector(`.${firstCapitalized}`).append(draggable);
+    }
+  });
+  // console.log(space.children);
+
+  // board game approach
+  // console.log(playerGameBoard.forEach((node) => console.log(node)));
+});
+
+rotateShipButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  placementDirection = !placementDirection;
+  console.log(placementDirection);
+
+  if (placementDirection === false) {
+    shipContainer.style.flexDirection = "column";
+    document.querySelectorAll(".default").forEach((draggable) => {
+      draggable.classList.remove("vertical");
+      draggable.classList.add("horizontal");
+    });
+  } else {
+    shipContainer.style.flexDirection = "row";
+    document.querySelectorAll(".default").forEach((draggable) => {
+      draggable.classList.remove("horizontal");
+      draggable.classList.add("vertical");
+    });
+  }
+});
 
 function initializeApp() {
   for (let i = 0; i < playerGameBoardObject.grid.length; i++) {
     let gridBox = document.createElement("div");
     gridBox.style.width = "10%";
     gridBox.style.height = "10%";
-    // gridBox.draggable = false;
     gridBox.style.border = "solid black .5px";
     gridBox.dataset.boxNumber = i;
     gridBox.classList.add("container");
     playerGameBoard.appendChild(gridBox);
   }
 
-  const draggables = document.querySelectorAll(".draggable");
   const containers = document.querySelectorAll(".container");
+  // let placementDirection = true;
+
   console.log(shipArray);
   draggables.forEach((draggable) => {
     draggable.addEventListener("dragstart", (e) => {
+      // Add DRagging Class for Eleemnts being Dragged
       draggable.classList.add("dragging");
       let targetShip =
         e.target.classList[1].split("")[0].toUpperCase() +
         e.target.classList[1].slice(1);
       let variableName = `default${targetShip}`;
       let foundElement = document.querySelector(`.${variableName}`);
-      e.target.style.height = `${foundElement.clientHeight}px`;
+      if (placementDirection === true) {
+        e.target.style.height = `${foundElement.clientHeight}px`;
+        e.target.style.width = `100%`;
+      }
+      if (placementDirection === false) {
+        e.target.style.width = `${foundElement.clientWidth}px`;
+        e.target.style.height = `100%`;
+      }
     });
 
     draggable.addEventListener("dragend", (e) => {
@@ -58,7 +139,6 @@ function initializeApp() {
       e.preventDefault();
 
       const draggable = document.querySelector(".dragging");
-      console.log(draggable.draggable);
       if (draggable.draggable === false) {
         return;
       }
@@ -81,7 +161,6 @@ function initializeApp() {
   });
 
   document.addEventListener("drop", (e) => {
-    console.log(e.target);
     try {
       let ship = shipArray.find(
         (ship) => ship.name === e.target.firstChild.dataset.shipName
@@ -89,9 +168,13 @@ function initializeApp() {
       if (e.target.firstChild) {
         e.target.firstChild.style.backgroundColor = "green";
         e.target.firstChild.draggable = false;
+
+        let desiredAxis =
+          placementDirection === true ? "Vertical" : "Horizontal";
+
         playerGameBoardObject.placeShip(
           ship,
-          "Vertical",
+          desiredAxis,
           Number(e.target.dataset.boxNumber)
         );
       }
@@ -102,12 +185,10 @@ function initializeApp() {
         }
       }
       for (let boxNumber of nonEmptyBoxes) {
-        console.log(boxNumber);
         let element = document.querySelector(
           `[data-box-number="${boxNumber}"]`
         );
         element.style.backgroundColor = "green";
-        // playerGameBoardObject.grid[boxNumber].backgroundColor = "green";
       }
     } catch (err) {
       console.log(err);
